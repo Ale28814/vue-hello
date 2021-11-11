b028049be8df11f6d810a6bedc645c0f6fb51301
Ale28814 <alened928@gmail.com>
2021-11-11 16:11:59 +0100

    Add exercise

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 // Stampare a schermo un messaggio in h1
 // Aggiungere una immagine presa da data
+
+const myApp = new Vue({
+    el: '#myApp',
+    data: {
+        title: 'Grotte di Frasassi',
+        centerClass: 'center',
+        imgContainer: 'container',
+        image: './img/grotte-di-frasassi.jpg',
+    }
+});
